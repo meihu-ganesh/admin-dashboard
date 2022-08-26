97a5f9aec0e3929c844b035a3a30191b7863317c
CSNR Ganesh <100460284+meihu-ganesh@users.noreply.github.com>
2022-08-26 20:09:13 +0530

    admin dashboard + profile

--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import Layout from "../../components/Layout";
+import "./Dashboard.css"
 
 
 export default function Dashboard({ history }) {
     // console.log(history);
     return (
         <Layout>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh',
-                }}
-            >
-
-                <div className="admin-title">Dashboard</div>
+            <div className="dashboard-screen">
+                <div className="admin-profile">
+                    <div className="profile-pic">
+                        <img className="admin-pic" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="profile photo" />
+                    </div>
+                    <div className="admin-info">
+                        <div className="adm-name-conti">
+                            <h3 className="admin-name">Admin Name</h3>
+                        </div>
+                        <div className="admin-desc">
+                            <p className="admin-post">President</p>
+                            <p className="admin-post">Contributor</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="router-btns">
+                    <div className="routing-conti">
+                        <button className="btn-route btn btn-primary">Certificate Generator</button>
+                    </div>
+                    <div className="routing-conti">
+                        <button className="btn-route btn btn-secondary">Data Update</button>
+                    </div>
+                </div>
             </div>
         </Layout>
 
